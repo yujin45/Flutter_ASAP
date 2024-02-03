82e1e3352cb5226a8c8076a1fbe0b1bdec62d0a2
JeongYujin <yujin45@sookmyung.ac.kr>
2024-02-03 13:55:14 +0900

    [yujin] test commit

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'onboardingPage/onboarding_screen.dart';
 void main() {
   runApp(MyApp());
 }
-
+//플러터.. 위젯 구성.. 괄호 속 괄호
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
